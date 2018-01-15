8cf7d73398cadf47d8f1c2567d196ab5786ec511
Kronecker <benjamin.grabiger@uni-jena.de>
2018-01-15 23:51:46 +0100

    Finish T12

--- solveTask12.cpp
+++ solveTask12.cpp
@@ -11,20 +11,20 @@
 
 using namespace std;
 
-
-
 double* jacobiIterOfBlockMatrixFourDiags2(double valLowBlockDiag,double valLowMinDiag,double valMainDiag, double valUpDiag,double valUpBlockDiag, int n, double f, double valBoundary);
 double* jacobiIterOfBlockMatrixFourDiagsPThread(double valLowBlockDiag,double valLowMinDiag,double valMainDiag, double valUpDiag,double valUpBlockDiag, int n, double f, double valBoundary, int procs);
+double* jacobiIterOfBlockMatrixFourDiagsPThreadBarrier(double valLowBlockDiag,double valLowMinDiag,double valMainDiag, double valUpDiag,double valUpBlockDiag, int n, double f, double valBoundary, int procs);
 void* subrJacobiThreads(void* param);
+void* subrJacobiThreadsBarrierCached(void* param);
 void solveTask12();
 
-
+int globalMaxIteration=2000;
 
 
 void solveTask12() {
 
     int n=1024;
-    double f=0;
+    double f=1;
     double valBoundary=0;
     double veloZero=200;
 
@@ -54,33 +54,10 @@ void solveTask12() {
     std::chrono::duration<double> elapsed=std::chrono::duration_cast<std::chrono::nanoseconds>(finish-start);
     std::cout<<"Threadless Jacobi finished after "<<elapsed.count()*1000<<"ms."<<std::endl;
 
-    bool saveToFile=false;
-
-
-    if(saveToFile) {
-
-        int index;
-        ofstream myfile;
-        myfile.open("single.dat");
-
-        for (int k = 0; k < n; k++) {
-            for (int i = 0; i < n; i++) {
-                index = k * n + i;
-                myfile<<k*h<<" "<<i*h<<" "<<solutionVector[index];
-                myfile << "\n";
-            }
-            myfile << "\n";
-        }
-        myfile.close();
-        std::cout<<"Results saved."<<std::endl;
-    }
-
-
 
     std::cout<<std::endl<<"Jacobi with PThreads started"<<std::endl;
     int procs[]={1,2,4,8,16,32};
     int procNum=6;
-
     for(int i=0;i<procNum;i++) {
         start = std::chrono::high_resolution_clock::now();
 
@@ -91,77 +68,39 @@ void solveTask12() {
         elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start);
         std::cout<<(procs[i]>10?"Calculation finished with ":"Calculation finished with  ")<<procs[i]<<" threads after "<<elapsed.count()*1000<<"ms."<<std::endl;
 
-        if (saveToFile) {
-
-            int index;
-            ofstream myfile;
-            myfile.open("threads.dat");
-
-            for (int k = 0; k < n; k++) {
-                for (int i = 0; i < n; i++) {
-                    index = k * n + i;
-                    myfile << k * h << " " << i * h << " " << solutionVector2[index];
-                    myfile << "\n";
-                }
-                myfile << "\n";
-            }
-            myfile.close();
-            std::cout << "Results saved." << std::endl;
-        }
-
-        double resi=0;
-        int nn=n*n;
-        for(int k=0;k>nn;k++) {
-            resi=fabs(solutionVector2[k]-solutionVector[k]);
-        }
-        std::cout<<resi<<std::endl;
-        delete(solutionVector);
       delete (solutionVector2);
 
     }
 
+    std::cout<<std::endl<<"Jacobi with PThreads Sync started"<<std::endl;
 
+    for(int i=0;i<procNum;i++) {
+        start = std::chrono::high_resolution_clock::now();
 
+        solutionVector2 = jacobiIterOfBlockMatrixFourDiagsPThreadBarrier(valLowBlockDiag, valLowMinDiag, valMainDiag, valUpDiag,
+                                                                         valUpBlockDiag, n, f, valBoundary, procs[i]);
 
+        finish = std::chrono::high_resolution_clock::now();
+        elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start);
+        std::cout<<(procs[i]>10?"Calculation finished with ":"Calculation finished with  ")<<procs[i]<<" threads after "<<elapsed.count()*1000<<"ms."<<std::endl;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        int nn=n*n;
+        double resi=0;
+        for(int i=0;i<nn;i++) {
+            resi+=fabs(solutionVector[i]-solutionVector2[i]);
+        }
+        std::cout<<resi<<std::endl;
+    }
+    delete(solutionVector);
 }
-
-
-
-
-
-
-
-
 
 double* jacobiIterOfBlockMatrixFourDiags2(double valLowBlockDiag,double valLowMinDiag,double valMainDiag, double valUpDiag,double valUpBlockDiag, int n, double f, double valBoundary) {
 
     double* actualIteration=new double[n*n]();
     double* lastIterSol=new double[n*n]();
     double* temp;
-    int maxIter=200;
-//    double tol=0.0001;
+    int maxIter=globalMaxIteration;
     int iteration=0;
-  //  double resi=tol+1;
 
     for(int i=0;i<n;i++) {
         actualIteration[i]=valBoundary;
@@ -181,33 +120,19 @@ double* jacobiIterOfBlockMatrixFourDiags2(double valLowBlockDiag,double valLowMi
     while(iteration<maxIter) {  // removed &&resi>tol to compare by iteration speed
 
         for(int k=1;k<nm1;k++) { // iterate through blocks
-
-
-
-
             for(int i=1;i<nm1;i++) {  // iterate in block
                 index=k*n+i;
                 actualIteration[index]=1/valMainDiag*(f-valLowBlockDiag*lastIterSol[index-n]-valLowMinDiag*lastIterSol[index-1]-valUpDiag*lastIterSol[index+1]-valUpBlockDiag*lastIterSol[index+n]);
             }
-
-
-
-
         }
-
         temp=actualIteration;
         actualIteration=lastIterSol;
         lastIterSol=temp;
-
         iteration++;
     }
     delete(actualIteration);
     return lastIterSol;
 }
-
-
-
-
 
 
 typedef struct messJacobiShared{
@@ -220,6 +145,8 @@ typedef struct messJacobiShared{
     double valUpBlockDiag;
     double f;
     int n;
+    int iteration;
+    int maxIteration;
 };
 
 typedef struct messJacobiPrivate{
@@ -235,10 +162,8 @@ double* jacobiIterOfBlockMatrixFourDiagsPThread(double valLowBlockDiag,double va
     double* actualIteration=new double[n*n]();
     double* lastIterSol=new double[n*n]();
     double* temp;
-    int maxIter=200;
-//    double tol=0.0001;
+    int maxIter=globalMaxIteration;
     int iteration=0;
-    //  double resi=tol+1;
 
     // prepare data with boundary values
     for(int i=0;i<n;i++) {
@@ -254,19 +179,13 @@ double* jacobiIterOfBlockMatrixFourDiagsPThread(double valLowBlockDiag,double va
         lastIterSol[(k+1)*n-1]=valBoundary;
     }
 
-
-
-    int nm1=n-1;
-    int index;
-
     // first and last block are boundary value only and do not need any computation
     // -> number ofblocks to iterate thorugh is n-2
 
     int numBlocksPerThread=(n-2)/procs;  // integer division gives number of blocks per thread to distribute, we also need the rest to
     int restBlocks=(n-2)-procs*numBlocksPerThread; //these blocks must be divided as well (1 per thread til none are left)
 
-
-    messJacobiShared mJShared={actualIteration, lastIterSol, valMainDiag, valLowBlockDiag, valLowMinDiag,valUpDiag, valUpBlockDiag,f,n};
+    messJacobiShared mJShared={actualIteration, lastIterSol, valMainDiag, valLowBlockDiag, valLowMinDiag,valUpDiag, valUpBlockDiag,f,n,0,maxIter};
 
     messJacobiPrivate* mJPrivates=new messJacobiPrivate[procs];
     mJPrivates[0].threadId=0;
@@ -283,9 +202,6 @@ double* jacobiIterOfBlockMatrixFourDiagsPThread(double valLowBlockDiag,double va
         restBlocks--;
     }
 
-
-
-    int r;
     pthread_t tid, *thread_id;
     thread_id=new pthread_t[procs];
 
@@ -304,7 +220,6 @@ double* jacobiIterOfBlockMatrixFourDiagsPThread(double valLowBlockDiag,double va
 
         }
 
-
         temp=mJShared.actualIteration;
         mJShared.actualIteration=mJShared.lastIterSol;
         mJShared.lastIterSol=temp;
@@ -316,11 +231,11 @@ double* jacobiIterOfBlockMatrixFourDiagsPThread(double valLowBlockDiag,double va
 }
 
 pthread_barrier_t iterationBarrier;
-int maxIterGlobal=200;
 double* jacobiIterOfBlockMatrixFourDiagsPThreadBarrier(double valLowBlockDiag,double valLowMinDiag,double valMainDiag, double valUpDiag,double valUpBlockDiag, int n, double f, double valBoundary, int procs) {
 
     double* actualIteration=new double[n*n]();
     double* lastIterSol=new double[n*n]();
+    int maxIter=globalMaxIteration;
 
     // prepare data with boundary values
     for(int i=0;i<n;i++) {
@@ -335,16 +250,13 @@ double* jacobiIterOfBlockMatrixFourDiagsPThreadBarrier(double valLowBlockDiag,do
         actualIteration[(k+1)*n-1]=valBoundary;
         lastIterSol[(k+1)*n-1]=valBoundary;
     }
-
-
     // first and last block are boundary value only and do not need any computation
-    // -> number ofblocks to iterate thorugh is n-2
+    // -> number of blocks to iterate thorugh is n-2
 
     int numBlocksPerThread=(n-2)/procs;  // integer division gives number of blocks per thread to distribute, we also need the rest to
     int restBlocks=(n-2)-procs*numBlocksPerThread; //these blocks must be divided as well (1 per thread til none are left)
 
-
-    messJacobiShared mJShared={actualIteration, lastIterSol, valMainDiag, valLowBlockDiag, valLowMinDiag,valUpDiag, valUpBlockDiag,f,n};
+    messJacobiShared mJShared={actualIteration, lastIterSol, valMainDiag, valLowBlockDiag, valLowMinDiag,valUpDiag, valUpBlockDiag,f,n,0,maxIter};
 
     messJacobiPrivate* mJPrivates=new messJacobiPrivate[procs];
     mJPrivates[0].threadId=0;
@@ -361,14 +273,13 @@ double* jacobiIterOfBlockMatrixFourDiagsPThreadBarrier(double valLowBlockDiag,do
         restBlocks--;
     }
 
-    int r;
     pthread_t tid, *thread_id;
     thread_id=new pthread_t[procs];
     pthread_barrier_init(&iterationBarrier,NULL,procs);
 
         for(int i=0;i<procs;i++) {
 
-            pthread_create(&tid, NULL, subrJacobiThreads, (void*) &mJPrivates[i]);
+            pthread_create(&tid, NULL, subrJacobiThreadsBarrierCached, (void*) &mJPrivates[i]);
             thread_id[i]=tid;
 
         }
@@ -378,9 +289,6 @@ double* jacobiIterOfBlockMatrixFourDiagsPThreadBarrier(double valLowBlockDiag,do
             pthread_join(thread_id[i],NULL);
 
         }
-
-
-
 
     delete(mJShared.actualIteration);
     return mJShared.lastIterSol;
@@ -404,31 +312,72 @@ void* subrJacobiThreads(void* param) {
             }
 
         }
+}
 
-
-
+void* subrJacobiThreadsBarrierCached(void* param) {
+    messJacobiPrivate *m=(messJacobiPrivate *) param;
+    // Sharing is caring, but caching is smashing.
+    double *actualIteration=m->mJShared->actualIteration, *lastIterSol=m->mJShared->lastIterSol;
+    double valLowMinDiag=m->mJShared->valLowMinDiag, valLowBlockDiag=m->mJShared->valLowBlockDiag, valMainDiag=m->mJShared->valMainDiag, valUpDiag=m->mJShared->valUpDiag,
+            valUpBlockDiag=m->mJShared->valUpBlockDiag, f=m->mJShared->f;
+    double *temp;
+    int nm1=m->mJShared->n-1, n=m->mJShared->n;
+    int startBlockIndex=m->startBlockIndex, endBlockIndex=m->endBlockIndex;
+    int index;
+    while(m->mJShared->iteration<m->mJShared->maxIteration) {
+        for (int k = startBlockIndex;
+             k <= endBlockIndex; k++) { // iterate through blocks -> divide into small for loops per thread
+            for (int i = 1; i < nm1; i++) {  // iterate in block
+                index = k * n + i;
+                actualIteration[index] = 1 / valMainDiag *
+                                                      (f - valLowBlockDiag *  lastIterSol[index - n] -
+                                                       valLowMinDiag * lastIterSol[index - 1] -
+                                                       valUpDiag * lastIterSol[index + 1] -
+                                                       valUpBlockDiag *    lastIterSol[index + n]);
+            }
+        }
+        pthread_barrier_wait(&iterationBarrier);
+        if(m->threadId==0) {  // I'm the Master.
+            m->mJShared->iteration++;
+        }
+        temp=actualIteration;
+        actualIteration=lastIterSol;
+        lastIterSol=temp;
+        pthread_barrier_wait(&iterationBarrier);
+    }
+    if(m->threadId==0) {  // I'm the Master.
+        m->mJShared->lastIterSol=lastIterSol;
+    }
 
 }
-void* subrJacobiThreadsBarrier(void* param) {
+// Dump
+void* subrJacobiThreadsBarrier(void* param) {  // deprecated
     messJacobiPrivate *m=(messJacobiPrivate *) param;
     int nm1=m->mJShared->n-1;
     int index;
-    for (int k = m->startBlockIndex;
-         k <= m->endBlockIndex; k++) { // iterate through blocks -> divide into small for loops per thread
-        for (int i = 1; i < nm1; i++) {  // iterate in block
-            index = k * m->mJShared->n + i;
-            m->mJShared->actualIteration[index] = 1 / m->mJShared->valMainDiag *
-                                                  (m->mJShared->f - m->mJShared->valLowBlockDiag *
-                                                                    m->mJShared->lastIterSol[index-m->mJShared->n] -
-                                                   m->mJShared->valLowMinDiag *
-                                                   m->mJShared->lastIterSol[index - 1] -
-                                                   m->mJShared->valUpDiag * m->mJShared->lastIterSol[index + 1] -
-                                                   m->mJShared->valUpBlockDiag *
-                                                   m->mJShared->lastIterSol[index + m->mJShared->n]);
+    while(m->mJShared->iteration<m->mJShared->maxIteration) {
+        for (int k = m->startBlockIndex;
+             k <= m->endBlockIndex; k++) { // iterate through blocks -> divide into small for loops per thread
+            for (int i = 1; i < nm1; i++) {  // iterate in block
+                index = k * m->mJShared->n + i;
+                m->mJShared->actualIteration[index] = 1 / m->mJShared->valMainDiag *
+                                                      (m->mJShared->f - m->mJShared->valLowBlockDiag *
+                                                                        m->mJShared->lastIterSol[index -
+                                                                                                 m->mJShared->n] -
+                                                       m->mJShared->valLowMinDiag *
+                                                       m->mJShared->lastIterSol[index - 1] -
+                                                       m->mJShared->valUpDiag * m->mJShared->lastIterSol[index + 1] -
+                                                       m->mJShared->valUpBlockDiag *
+                                                       m->mJShared->lastIterSol[index + m->mJShared->n]);
+            }
         }
-
+        pthread_barrier_wait(&iterationBarrier);
+        if(m->threadId==0) {  // I'm the Master.
+            m->mJShared->iteration++;
+            double *temp=m->mJShared->actualIteration;
+            m->mJShared->actualIteration=m->mJShared->lastIterSol;
+            m->mJShared->lastIterSol=temp;
+        }
+        pthread_barrier_wait(&iterationBarrier);
     }
-    pthread_barrier_wait(&iterationBarrier);
-
 }
-
